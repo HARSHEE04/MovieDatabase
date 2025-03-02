ebb835d0f1eeb8bbf39c22ba172a7feaeb18aa2e
HARSHEE04 <harshetasharma5@gmail.com>
2025-03-02 12:14:00 -0500

    Edited the repository class to add a custom query to help search movie by name. Also edited the service class to all users to updateMovie, and search movie by the name using the custom query.

--- src/main/java/com/example/moviedatabase/Repository/MovieRepository.java
+++ src/main/java/com/example/moviedatabase/Repository/MovieRepository.java
@@ -4,8 +4,11 @@ import com.example.moviedatabase.Model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
+    List<Movie> findByNameContainingIgnoreCase(String name);
 
 
 }
--- src/main/java/com/example/moviedatabase/Service/MovieService.java
+++ src/main/java/com/example/moviedatabase/Service/MovieService.java
@@ -3,10 +3,13 @@ package com.example.moviedatabase.Service;
 import com.example.moviedatabase.Model.Movie;
 import com.example.moviedatabase.Repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class MovieService {
 
     //allows us to interact with the movies collection
@@ -28,9 +31,29 @@ public class MovieService {
     }
 
     //getMovieByID( int ID) //returns the movie object
+    public Movie getMovieById(int id){
+        Optional<Movie> movie = movieRepository.findById(id);
+        return movie.orElse(null); //returns the movie or else returns null
+    }
 
 
     //updateMovie(Movie movie)
+    public Movie updateMovie(Movie movie) {
+        Optional<Movie> existingMovie = movieRepository.findById(movie.getId());
+
+        if (existingMovie.isPresent()) {
+            Movie updatedMovie = existingMovie.get();
+            updatedMovie.setName(movie.getName());
+            updatedMovie.setGenre(movie.getGenre());
+            updatedMovie.setLanguage(movie.getLanguage());
+            updatedMovie.setDuration(movie.getDuration());
+            updatedMovie.setPrice(movie.getPrice());
+
+            return movieRepository.save(updatedMovie); // Save the updated movie
+        } else {
+            throw new RuntimeException("Movie not found with id: " + movie.getId());
+        }
+    }
 
 
     //deleteMovie
@@ -38,6 +61,9 @@ public class MovieService {
         movieRepository.deleteById(movieId);
     }
     //searchMoviesByName( string name)
+    public List<Movie> searchMoviesByName(String name) {
+        return movieRepository.findByNameContainingIgnoreCase(name);
+    }
 
 
 }
